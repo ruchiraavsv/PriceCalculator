9af8d0f7b28b86e5b62aae3b38bbe0744af4e2c2
RuchiraVitharama <ruchira.15@cse.mrt.ac.lk>
2021-02-19 18:43:50 +0530

    project skeleton

---
+++ src/test/java/com/price/calculator/v1/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.price.calculator.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
